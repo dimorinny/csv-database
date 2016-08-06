0461e2afc42e9923e4bfdc744087a851bd946962
dimorinny <didika914@gmail.com>
2016-08-06 22:16:04 +0300

    fix description

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject csv-database "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "Test clojure project"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
